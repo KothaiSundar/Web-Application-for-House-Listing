0e18034cc8281e68981d492b8fa69551b10db039
kothai <kothai056@gmail.com>
2024-03-03 15:16:41 +0100

    final changes

--- src/services/HousingApiServices.js
+++ src/services/HousingApiServices.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-// Set up the base URL and API key from environment variables
-// const API_BASE_URL = process.env.HOUSING_APP_API_BASE_URL;
-// const API_KEY = process.env.HOUSING_APP_API_KEY;
+
 const API_BASE_URL = "https://api.intern.d-tt.nl/api/houses";
 const API_KEY = "ji1kyAep4Ubh2EZ5HOn7auSPIR_rLKD-";
 
